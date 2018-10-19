de37ac1509341242ae423db07627239695cdee5a
youngstruggle <febrian.nana@gmail.com>
2018-10-19 10:33:30 +0700

    initial

--- RMNDR/src/main/java/dao/RcustomerDaoImpl.java
+++ RMNDR/src/main/java/dao/RcustomerDaoImpl.java
@@ -2,6 +2,9 @@ package dao;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
@@ -11,15 +14,24 @@ import entities.Rcustomer;
 @Repository
 public class RcustomerDaoImpl implements RcustomerDao {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(RcustomerDaoImpl.class);
+
 	@Override
 	public List<Rcustomer> getCustomerList() {
 		List<Rcustomer> customerlist = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
-			Session session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			customerlist = session.createCriteria(Rcustomer.class).list();
-		} catch (Exception e) {
-			e.printStackTrace();
+			session.getTransaction().commit();
+			LOGGER.debug("Get customer");
+			
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			LOGGER.error("Error Bung {}", e.getMessage());
+		} finally {
+			session.close();
+			LOGGER.info("Transaction Finish");
 		}
 		return customerlist;
 	}
@@ -27,78 +39,101 @@ public class RcustomerDaoImpl implements RcustomerDao {
 	@Override
 	public Rcustomer deleteCustomer(String customerId) {
 		Rcustomer customer = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
-			Session session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			customer = (Rcustomer) session.get(Rcustomer.class, customerId);
 			if (customer != null) {
 				session.delete(customer);
 				session.getTransaction().commit();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.debug("Data sucessfully deleted!");
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			LOGGER.error("Error Delete {}", e.getMessage());
+		} finally {
+			session.close();
+			LOGGER.info("Transaction selesai");
 		}
 		return customer;
 	}
 
 	@Override
 	public Rcustomer createCustomer(Rcustomer customer) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
 			if (customer != null) {
-				Session session = HibernateUtil.getSessionFactory()
-						.openSession();
 				session.beginTransaction();
 				session.save(customer);
 				session.getTransaction().commit();
+				LOGGER.debug("Data sucessfully created!");
 			} else {
-				System.out.println("Ada yang null!");
+				LOGGER.debug("Some data is null");				
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			LOGGER.error("Error Delete {}", e.getMessage());
+		} finally {
+			session.close();
+			LOGGER.info("Transaction End");
 		}
-
 		return customer;
 	}
 
 	@Override
 	public Rcustomer getCustomerById(String userId) {
 		Rcustomer customer = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
-			Session session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			customer = (Rcustomer) session.get(Rcustomer.class, userId);
 			session.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
+			LOGGER.debug("Get customer Byid");		
+
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			LOGGER.error("Error Bung {}", e.getMessage());
+		} finally {
+			session.close();
+			LOGGER.info("Transaction end");
 		}
 		return customer;
 	}
 
 	@Override
 	public Rcustomer updateCustomerObj(Rcustomer customer) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		try {
-			Session session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			if (customer != null) {
 				session.update(customer);
 				session.getTransaction().commit();
+				LOGGER.debug("Data succesfully updated");
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			LOGGER.error("Error Bung {}", e.getMessage());
+		} finally {
+			session.close();
+			LOGGER.info("Transaction end");
 		}
 		return customer;
 	}
 
 	@Override
 	public List<Rcustomer> getCustomerListByQuery(String sql) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		List<Rcustomer> customerlist = null;
 		try {
-			Session session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			customerlist = session.createQuery(sql).list();
 			session.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (HibernateException e) {
+			session.getTransaction().rollback();
+			LOGGER.error("Error Bung {}", e.getMessage());
+		} finally {
+			session.close();
+			LOGGER.info("Transaction end");
 		}
 		return customerlist;
 	}
